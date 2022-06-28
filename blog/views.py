aca70713d98463e2d7e83b4e7524d2b05e169869
Diego Posada <dchavarria@playvox.com>
2022-06-28 20:06:39 +0000

    Finish fetch and React hooks

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from blog.models import Post
 from django.shortcuts import redirect
 from blog.forms import CommentForm
 import logging
-
+from django.urls import reverse
 
 logger = logging.getLogger(__name__)
 # Create your views here.
@@ -42,4 +42,6 @@ def get_ip(request):
   return HttpResponse(request.META['REMOTE_ADDR'])
 
 def post_table(request):
-    return render(request, "blog/post-table.html")
+    return render(
+        request, "blog/post-table.html", {"post_list_url": reverse("post-list")}
+    )
